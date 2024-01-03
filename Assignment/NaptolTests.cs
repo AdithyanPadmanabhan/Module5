3f0dd6fd343e3a3177394efb66086030086018eb
AdithyanPadmanabhan <197160@ust.com>
2024-01-03 17:23:20 +0530

    Assignment andDaily works-3-1-24

---
+++ Assignment/NaptolTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Playwright.NUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    [TestFixture]
+    internal class NaptolTests :PageTest
+    {
+        [SetUp]
+
+       
+
+        public async Task SetUp()
+        {
+            Console.WriteLine("opened browser");
+            await Page.GotoAsync("https://www.naaptol.com/");
+            Console.WriteLine("Page Loaded");
+        }
+        [Test]
+
+        public async Task ProductTest()
+        {
+
+            await Page.FillAsync(selector: "#header_search_text", value: "eyewear");
+            await Console.Out.WriteLineAsync("typed");
+
+            await Page.Locator("(//div[@class='search'])[2]").ClickAsync();
+            await Console.Out.WriteLineAsync("searched");
+
+        }
+        }
+}
--- PlayWrightNunit/EATests.cs
+++ PlayWrightNunit/EATests.cs
@@ -12,35 +12,63 @@ namespace PlayWrightNunit
     internal class EATests : PageTest
     {
 
-        [Test]
 
-        public async Task LoginTest()
+        [SetUp]
+
+        public async Task SetUp()
         {
             Console.WriteLine("opened browser");
             await Page.GotoAsync("http://eaapp.somee.com/");
             Console.WriteLine("Page Loaded");
+        }
+        [Test]
+
+        public async Task LoginTest()
+        {
+           
 
             await Page.GetByText("Login").ClickAsync();
 
+
+/*
+            var linklogin = Page.Locator(selector: "text=Login"); //another method
+
+            await linklogin.ClickAsync();*/
+
             await Console.Out.WriteLineAsync("Link Clicked");
             await Expect(Page).ToHaveURLAsync("http://eaapp.somee.com/Account/Login");
-            await Page.Locator("#UserName").FillAsync(value: "admin");
-            await Page.Locator("#Password").FillAsync(value: "password");
+           /* await Page.Locator("#UserName").FillAsync(value: "admin");
+            await Page.Locator("#Password").FillAsync(value: "password");*/
+
+            //another method
+
+            await Page.FillAsync(selector: "#UserName", value: "admin");
+
+            await Page.FillAsync(selector: "#Password", value: "password");
+
             await Console.Out.WriteLineAsync("Typed");
 
-            //  await Page.Locator("//input[@value='Log in']").ClickAsync();
-            var btnLogin = Page.Locator(selector: "input", new PageLocatorOptions
+            //  await Page.Locator("//input[@value='Log in']").ClickAsync(); //another method
+
+
+            var btnlogin = Page.Locator(selector: "input", new PageLocatorOptions //another method
             {
 
-                HasTextString ="Log in"
+                HasTextString = "Log in"
             });
 
-            await btnLogin.ClickAsync();
+
+
+
+           /* var btnlogin = Page.Locator(selector: "text=Log in"); //another method*/
+
+            await btnlogin.ClickAsync();
 
             await Console.Out.WriteLineAsync("Clicked");
 
-            await Expect(Page).ToHaveTitleAsync("Home - Execute Automation Employee App");
+           // await Expect(Page).ToHaveTitleAsync("Home - Execute Automation Employee App");
 
+            await Expect(Page.Locator(selector:"text='Hello admin!'")).ToBeVisibleAsync();
         }
     }
 }
